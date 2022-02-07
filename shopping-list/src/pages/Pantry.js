427ba4cde90373071d04221de62abbdde430a0a7
Peter DuCharme <peter.ducharme@coding.partners>
2022-02-07 14:48:28 -0500

    Stub pages

---
+++ shopping-list/src/pages/Pantry.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Pantry() {
+    return (
+        <div>
+            <h1>Pantry</h1>
+        </div>
+    )
+}
---
+++ shopping-list/src/pages/Shoppinglist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Shoppinglist() {
+    return (
+        <div>
+            <h1>Shoppinglist</h1>
+        </div>
+    )
+}
